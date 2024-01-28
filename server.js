d1c22fbf0e7b6fd011376d0d92c889144b1afd4f
maheye <u277721157@byui.edu>
2024-01-28 10:17:00 +0800

    week01

--- server.js
+++ server.js
@@ -6,9 +6,17 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+/* ***********************
+ * View engine and templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
@@ -27,4 +35,9 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
 })
