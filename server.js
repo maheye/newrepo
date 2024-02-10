15318af956385599ca3418aaffefed647ca26964
maheye <u277721157@byui.edu>
2024-02-10 17:37:47 +0800

    updates

--- server.js
+++ server.js
@@ -10,10 +10,45 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-const baseController = require ("./controllers/baseController")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute")
+const utilities = require("./utilities/index")
+const session = require("express-session")
+const pool = require("./database/")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
 /* ***********************
- * View engine and templates
+ * Middleware
+ * ************************/
+app.use(session({
+    store: new (require('connect-pg-simple')(session))({
+        createTableIfMissing: true,
+        pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+    res.locals.messages = require('express-messages')(req, res)
+    next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(cookieParser())
+
+app.use(utilities.checkJWTToken)
+
+/* ***********************
+ * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
@@ -24,11 +59,48 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 
-// Inventory routes
-app.use("inv", inventoryRoute)
+// Index route
+// app.get("/", function(req, res) {
+//   res.render("index", {title: "Home"})
+// })
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
-//Index route
-app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", utilities.handleErrors(inventoryRoute))
+app.use("../../inv", utilities.handleErrors(inventoryRoute))
+
+// Account routes
+app.use("/account", utilities.handleErrors(accountRoute))
+
+// Intentional Server Error
+// app.get("/footer", utilities.handleErrors(baseController.buildFooter))
+app.use("/inv", utilities.handleErrors(baseController))
+
+
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+    next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+    let nav = await utilities.getNav()
+    console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+    if(err.status == 404) { 
+        message = err.message
+    } else {
+        message = 'Oh no! There was a crash. Maybe try a different route?'
+    }
+    res.render("errors/error", {
+        title: err.status || 'Server Error',
+        message,
+        nav
+    })
+})
 
 /* ***********************
  * Local Server Information
@@ -41,10 +113,5 @@ const host = process.env.HOST
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
-
-//Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
+    console.log(`app listening on ${host}:${port}`)
 })
